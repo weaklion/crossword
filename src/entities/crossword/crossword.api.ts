f81940f35c67673719c27e2031627e2c56363b68
Ryan <ryan@newming.co.kr>
2025-01-17 18:26:40 +0900

    ⚡️ (improve): api 추가

--- src/entities/crossword/crossword.api.ts
+++ src/entities/crossword/crossword.api.ts
@@ -1,1 +1,23 @@
-export const getWord = async () => {};
+export const getWord = async () => {
+  const baseUrl = "https://stdict.korean.go.kr/api/search.do";
+
+  const key = process.env.KEY ?? "";
+
+  const params = {
+    key: key,
+    q: "사랑",
+    req_type: "json",
+    num: "10",
+    advanced: "y",
+    method: "include",
+  };
+
+  const paramsQueryString = new URLSearchParams(params).toString();
+  const url = `${baseUrl}?${paramsQueryString}`;
+
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  console.log(data, "data");
+};
